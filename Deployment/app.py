f662bbf4d1f51d672a42bab9a45429875dedf216
melbinmartin <melbinmartin1111@gmail.com>
2023-03-16 15:08:08 +0530

    first commit

---
+++ Deployment/app.py
@@ -0,0 +1,62 @@
+import time
+import streamlit as st
+import numpy as np
+from PIL import Image
+import urllib.request
+from utils import *
+
+labels = gen_labels()
+
+html_temp = '''
+  <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; margin-top: -50px">
+    <div style = "display: flex; flex-direction: row; align-items: center; justify-content: center;">
+     <center><h1 style="color: #000; font-size: 50px;"><span style="color: #0e7d73">Smart </span>Garbage</h1></center>
+    <img src="https://cdn-icons-png.flaticon.com/128/1345/1345823.png" style="width: 0px;">
+    </div>
+    <div style="margin-top: -20px">
+    <img src="https://i.postimg.cc/W3Lx45QB/Waste-management-pana.png" style="width: 400px;">
+    </div>  
+    </div>
+    '''
+
+st.markdown(html_temp, unsafe_allow_html=True)
+html_temp = '''
+    <div>
+    <center><h3 style="color: #008080; margin-top: -20px">Check the type here </h3></center>
+    </div>
+    '''
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.markdown(html_temp, unsafe_allow_html=True)
+opt = st.selectbox("How do you want to upload the image for classification?\n", ('Please Select', 'Upload image via link', 'Upload image from device'))
+if opt == 'Upload image from device':
+    file = st.file_uploader('Select', type = ['jpg', 'png', 'jpeg'])
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+    if file is not None:
+        image = Image.open(file)
+
+elif opt == 'Upload image via link':
+
+  try:
+    img = st.text_input('Enter the Image Address')
+    image = Image.open(urllib.request.urlopen(img))
+    
+  except:
+    if st.button('Submit'):
+      show = st.error("Please Enter a valid Image Address!")
+      time.sleep(4)
+      show.empty()
+
+try:
+  if image is not None:
+    st.image(image, width = 300, caption = 'Uploaded Image')
+    if st.button('Predict'):
+        img = preprocess(image)
+
+        model = model_arc()
+        model.load_weights("../weights/modelnew.h5")
+
+        prediction = model.predict(img[np.newaxis, ...])
+        st.info('Hey! The uploaded image has been classified as " {} product " '.format(labels[np.argmax(prediction[0], axis=-1)]))
+except Exception as e:
+  st.info(e)
+  pass
---
+++ Deployment/utils.py
@@ -0,0 +1,64 @@
+from tensorflow.keras.models import Sequential
+from keras.layers import Conv2D, Flatten, MaxPooling2D, Dense, Dropout, SpatialDropout2D
+from tensorflow.keras.losses import sparse_categorical_crossentropy, binary_crossentropy
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+from PIL import Image
+
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '1'
+
+def gen_labels():
+    train = '../Data/Train'
+    train_generator = ImageDataGenerator(rescale = 1/255)
+
+    train_generator = train_generator.flow_from_directory(train,
+                                                        target_size = (300,300),
+                                                        batch_size = 32,
+                                                        class_mode = 'sparse')
+    labels = (train_generator.class_indices)
+    labels = dict((v,k) for k,v in labels.items())
+
+    return labels
+
+def preprocess(image):
+    image = np.array(image.resize((300, 300), Image.ANTIALIAS))
+    image = np.array(image, dtype='uint8')
+    image = np.array(image)/255.0
+
+    return image
+
+def model_arc():
+    model = Sequential()
+
+    # Convolution blocks
+    model.add(Conv2D(32, kernel_size=(3,3), padding='same', input_shape=(300,300,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=2))
+
+    model.add(Conv2D(64, kernel_size=(3,3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=2))
+
+    model.add(Conv2D(32, kernel_size=(3,3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=2))
+
+    # Classification layers
+    model.add(Flatten())
+
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(32, activation='relu'))
+
+    model.add(Dropout(0.2))
+    model.add(Dense(6, activation='softmax'))
+
+    # Enable OneDNN optimizations
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    
+
+    return model
